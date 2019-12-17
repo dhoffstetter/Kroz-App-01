5932125a9a52a91d38dbb51208ae233c8483f287
Diane Hoffstetter <dhoffstetter@mac.com>
2019-12-17 05:50:05 -0800

    Initial Commit

---
+++ Kroz-App-01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kroz-App-01
+//
+//  Created by Diane Hoffstetter on 12/17/19.
+//  Copyright © 2019 Dumb Blonde Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
